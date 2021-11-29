d3e259cd5e9c963f6e9d62ce599d33682393aa38
Bram Scheepers <b.scheepers1@student.avans.nl>
2021-11-29 12:33:51 +0100

    #3 Added merge sort functionality

--- CLISort/main.cpp
+++ CLISort/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cstdlib>
-
+#include "mergeSort.h"
 
 int main() {
         int numbers[100];
@@ -11,6 +11,8 @@ int main() {
     for (int i = 0; i < 100; i++) {
         std::cout << numbers[i] << std::endl;
     }
+
+    mergeSort();
 
     return 0;
 }
---
+++ CLISort/mergeSort.cpp
@@ -0,0 +1,6 @@
+#include "mergeSort.h"
+
+void mergeSort()
+{
+	std::cout << "Merge sort: Array sorted." << std::endl;
+}
